ec8e11b89f9d58ca5525bf4cf20b638e74fbbf50
zero <1139271795@example.com>
2020-05-05 22:28:04 +0800

    feat: 第二次小练习完成

--- task0002-2/js/utl.js
+++ task0002-2/js/utl.js
@@ -140,6 +140,7 @@ console.log(getObjectLength(obj)); // 3
 function domQuery(selector, root) {
     var text;
     var elements = [];
+    //if root is not defined, root = document
     if (!root) {
         root = document;
     }
@@ -150,12 +151,15 @@ function domQuery(selector, root) {
         text = selector.replace(/^\./, "");
         elements = root.getElementsByClassName(text);
     } else if ((selector.charAt(0) === "[") && (selector.charAt(selector.length - 1) === "]")) {
+        //get all the elements
         var eles = root.getElementsByTagName("*");
+        //delete "[" and "]"
         selector = selector.replace(/^\[/, "");
         selector = selector.replace(/\]$/, "");
         var texts = selector.split("=");
         var attr = texts[0];
         var value = texts[1];
+        //有属性值的情况
         if (texts[1]) {
             for (var i = 0, length1 = eles.length; i < length1; i++) {
                 if (eles[i].hasAttribute(attr)) {
@@ -165,6 +169,7 @@ function domQuery(selector, root) {
                 }
             }
         }
+        //没有属性值
         else {
             for (var i = 0, length1 = eles.length; i < length1; i++) {
                 if (eles[i].hasAttribute(attr)) {
@@ -178,8 +183,10 @@ function domQuery(selector, root) {
     return elements;
 }
 function $(selector) {
+    //multiple queries
     var result = [];
     if (selector.indexOf(" ") !== -1) {
+        //split selector by space
         var selectors = selector.split(" ");
         parents = domQuery(selectors[0]);
         for (var i = 1, length1 = selectors.length; i < length1; i++) {
@@ -191,6 +198,7 @@ function $(selector) {
         }
         result = parents;
     }
+    //single query
     else {
         var result = domQuery(selector, document);
     }
@@ -212,7 +220,7 @@ function addEvent(element, event, listener) {
 
 // 移除element对象对于event事件发生时执行listener的响应
 function removeEvent(element, event, listener) {
-    if (element.removeEventListener) { 
+    if (element.removeEventListener) { //标准
         element.removeEventListener(event, listener, false);
     }
 }
@@ -304,15 +312,19 @@ if (document.cookie.length>0)
 return "";
 }
 
-// 学习Ajax，并尝试自己封装一个Ajax方法。实现如下方法：
+// 习Ajax，并尝试自己封装一个Ajax方法。实现如下方法：
 
 function ajax(url, options) {
+    //新建一个XHR对象
     var xmlhttp;
     if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp = new XMLHttpRequest();
     } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")   ;
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
+    //若没有设置type，则默认为get
 
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         options.onsuccess();
